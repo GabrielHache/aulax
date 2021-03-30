f5f9a5f167da391d739996d6d43bd53b8ad0ac86
GabrielHache <80431306+GabrielHache@users.noreply.github.com>
2021-03-29 21:47:07 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+function submit_form_login(){
+    
+   if  (document.form_login.input_email.value == 'tiao@gmail.com') {
+   if  (document.form_login.input_senha.value == '12345') {
+  alert("Cadastro");
+
+} else {
+    alert("você erro sua senha")
+    }
+
+} else {
+        alert("E-mail já cadastrado")
+}
+}
+
+
+
+  
+ 
